deee063c1ab8e4cf251fb3778cf4390de909a7ff
axdjuraev <axdjuraev@gmail.com>
2023-10-11 23:33:54 +0500

    ref (dialects):  better descriptive text

--- pydantic_handler_converter/dialecsts.py
+++ pydantic_handler_converter/dialecsts.py
@@ -2,18 +2,18 @@ from abc import ABC
 
 
 class BaseDialects(ABC):
-    INPUT = 'input'
-    INPUT_STR = 'input string for `{field_name}`'
-    INPUT_INT = 'input number for `{field_name}`'
-    INPUT_FLOAT = 'input number for `{field_name}`'
-    CHOOSE = 'choose value of `{field_name}`'
-    CHOOSE_FROM_ENUM = 'choose value of `{field_name}`'
-    CONTENT_TYPE_NOT_ALLOWED = 'content type is not allowed, please choose value from enum'
-    CHOOSE_FROM_ENUM_OR_INPUT = 'choose or input value of `{field_name}`'
-    BACK_BUTTON = 'back'
-    READY_BUTTON = 'ready'
+    INPUT = 'Enter a valuef for `{field_name}`'
+    INPUT_STR = 'Enter a string for `{field_name}`'
+    INPUT_INT = 'Enter a number for `{field_name}`'
+    INPUT_FLOAT = 'Enter a decimal number for `{field_name}`'
+    CHOOSE = 'Select a value for `{field_name}`'
+    CHOOSE_FROM_ENUM = 'Choose from options for `{field_name}`'
+    CONTENT_TYPE_NOT_ALLOWED = 'Invalid content type, please select from the available options'
+    CHOOSE_FROM_ENUM_OR_INPUT = 'Select or input a value for `{field_name}`'
+    BACK_BUTTON = 'Back'
+    READY_BUTTON = 'Ready'
     READY_BUTTON_DATA = 'ready'
-    SKIP_BUTTON = 'skip'
+    SKIP_BUTTON = 'Skip'
     SKIP_STEP_DATA = 'skip_step'
 
 
