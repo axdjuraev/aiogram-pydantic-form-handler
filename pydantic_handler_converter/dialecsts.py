6cbd7942f29ab4afb8732975d7cbd555a64d05a3
axdjuraev <axdjuraev@gmail.com>
2023-10-11 23:39:13 +0500

    feat (dialects): RuDialects

--- pydantic_handler_converter/dialecsts.py
+++ pydantic_handler_converter/dialecsts.py
@@ -20,3 +20,17 @@ class BaseDialects(ABC):
 class EnDialects(BaseDialects):
     pass
 
+
+class RuDialects(BaseDialects):
+    INPUT = 'Введите значение для `{field_name}`',
+    INPUT_STR = 'Введите строку для `{field_name}`',
+    INPUT_INT = 'Введите число для `{field_name}`',
+    INPUT_FLOAT = 'Введите десятичное число для `{field_name}`',
+    CHOOSE = 'Выберите значение для `{field_name}`',
+    CHOOSE_FROM_ENUM = 'Выберите из доступных вариантов для `{field_name}`',
+    CONTENT_TYPE_NOT_ALLOWED = 'Недопустимый тип контента, выберите из доступных вариантов',
+    CHOOSE_FROM_ENUM_OR_INPUT = 'Выберите или введите значение для `{field_name}`',
+    BACK_BUTTON = 'Назад',
+    READY_BUTTON = 'Готово',
+    SKIP_BUTTON = 'Пропустить',
+
