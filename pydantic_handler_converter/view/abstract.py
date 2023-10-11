06128b2ab46db0db20117fbd671bbff9c16f583c
axdjuraev <axdjuraev@gmail.com>
2023-10-12 00:23:56 +0500

    feat (AbstractView:bind): logger

--- pydantic_handler_converter/view/abstract.py
+++ pydantic_handler_converter/view/abstract.py
@@ -2,14 +2,16 @@ from abc import abstractmethod
 from types import MethodType
 from typing import Any
 from aiogram import Router
-
 from aiogram.fsm.context import FSMContext
+
+from pydantic_handler_converter.field_factory import logger
 from ..types import TEvent, BaseSingleHandler
 
 
 class AbstractView(BaseSingleHandler):
     def bind(self, elem):
         self.__call__ = MethodType(self.__call__, elem)
+        logger.debug(f"[{self.__class__.__name__}][bind]: {locals()=};")
 
     @abstractmethod
     async def __call__(self, self_, event: TEvent, state: FSMContext) -> Any:
