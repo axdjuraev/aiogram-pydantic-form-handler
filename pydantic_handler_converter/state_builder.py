6679cc3f638124ced1686e8d951b475ee97232d8
axdjuraev <axdjuraev@gmail.com>
2023-10-14 20:05:27 +0500

    feat (SchemaStates): union state creation

--- pydantic_handler_converter/state_builder.py
+++ pydantic_handler_converter/state_builder.py
@@ -1,7 +1,11 @@
-from typing import Type
-from inspect import isclass
+from types import UnionType
+from typing import Union, Type, get_origin, get_args
 from aiogram.fsm.state import State, StatesGroup
 from pydantic import BaseModel
+
+
+def _is_union(obj):
+    return get_origin(obj) in {Union, UnionType}
 
 
 class SchemaStates(StatesGroup):
@@ -13,12 +17,15 @@ class SchemaStates(StatesGroup):
         self = self or cls(schema.__name__)
 
         for field in schema.__fields__.values():
-            if not isclass(field.type_) or not issubclass(field.type_, BaseModel):
+            if not _is_union(field.type_) and not issubclass(field.type_, BaseModel):
                 setattr(self, field.name, State(field.name, self._base_name))
                 continue
                 
+            schemas = get_args(field.type_) if _is_union(field.type_) else (field.type_,)
             setattr(self, field.name, cls(f"{self._base_name}.{field.name}"))
-            cls.create(field.type_, getattr(self, field.name))
+
+            for schema in schemas:
+                cls.create(schema, getattr(self, field.name))
 
         return self
 
