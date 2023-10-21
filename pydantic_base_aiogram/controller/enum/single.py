2b1ca0f4e578b3efa07312d0f8ac07745ba45c2b
axdjuraev <axdjuraev@gmail.com>
2023-10-21 23:38:31 +0500

    ref: SingleValueEnumController

--- pydantic_base_aiogram/controller/enum/single.py
+++ pydantic_base_aiogram/controller/enum/single.py
@@ -1,14 +1,16 @@
 from aiogram import types
 from aiogram.fsm.context import FSMContext
 from pydantic_base_aiogram.abstract_handler import AbstractPydanticFormHandlers as THandler
+from pydantic_base_aiogram.exceptions import DataValidationError
 from pydantic_base_aiogram.types import Event
 
 from .base import BaseEnumController
 
 
 class SingleValueEnumController(BaseEnumController):
-    async def item_selected_handler(self, self_: THandler, cq: types.CallbackQuery, state: FSMContext):
-        _, value = str(cq.data).split(":")
-        await self._setvalue(self._validate_data(value), state)
-        return await self_.next(Event(cq), state, self.step_name)  # type: ignore
+    async def item_selected_handler(self, _: THandler, event: Event[types.CallbackQuery], state: FSMContext):
+        try:
+             return str(event._event.data).split(":")[1]
+        except IndexError:
+            raise DataValidationError(self.dialects.INVALID_TYPE_DATA)
 
