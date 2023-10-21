0b4bcb51df211c4fdd5ba74d5efd768b8ecb86a7
axdjuraev <axdjuraev@gmail.com>
2023-10-21 23:32:44 +0500

    ref: BaseEnumController

--- pydantic_base_aiogram/controller/enum/base.py
+++ pydantic_base_aiogram/controller/enum/base.py
@@ -1,5 +1,4 @@
 from abc import abstractmethod
-from typing import Union
 from aiogram import F, Router, types
 from aiogram.filters.state import StateFilter
 from aiogram.fsm.context import FSMContext
@@ -15,26 +14,22 @@ class BaseEnumController(BaseController):
         super().__init__(field=field, *args, **kwargs)
         self.is_string_allowed = is_string_allowed
 
-    async def __call__(self, self_: THandler, event: Union[types.Message, types.CallbackQuery], state: FSMContext):
-        if isinstance(event, types.Message):
-            if not self.is_string_allowed:
+    async def format_data(self, self_: THandler, event: Event, state: FSMContext):
+        if isinstance(event._event, types.Message):
+            if not self.is_string_allowed or not event._event.text:
                 return await event.answer(self.dialects.CONTENT_TYPE_NOT_ALLOWED) 
-            return await self.message_handler(self_, event, state)
+            return event._event.text
             
         return await self.item_selected_handler(self_, event, state) 
 
     def _validate_data(self, data):
         return getattr(self.field.type_, data)
 
-    async def message_handler(self, self_: THandler, message: types.Message, state: FSMContext):
-        await self._setvalue(message.text, state)
-        return await self_.next(Event(message), state, self.step_name)  # type: ignore
-
     def register2router(self, router: Router) -> Router:
         router.callback_query(StateFilter(self.state), F.data.startswith(self.item_callback_data))(self.__call__)
         return super().register2router(router)
     
     @abstractmethod
-    async def item_selected_handler(self, _: THandler, cq: types.CallbackQuery, state: FSMContext):
+    async def item_selected_handler(self, _: THandler, event: Event[types.CallbackQuery], state: FSMContext):
         raise NotImplementedError
 
