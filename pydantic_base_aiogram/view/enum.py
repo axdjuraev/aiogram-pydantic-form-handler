8bcfa63f5a60d89fcabfa1ec7bfe96374ffdea1b
axdjuraev <axdjuraev@gmail.com>
2023-10-22 01:22:11 +0500

    ref (EnumView): add extra_keys

--- pydantic_base_aiogram/view/enum.py
+++ pydantic_base_aiogram/view/enum.py
@@ -1,6 +1,3 @@
-from enum import Enum
-from typing import Optional, Type
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from pydantic.fields import ModelField
 
 from pydantic_base_aiogram.types import DescriptiveEnum
@@ -17,22 +14,14 @@ class EnumView(BaseView):
         return self.dialects.CHOOSE_FROM_ENUM if not self.is_string_allowed else self.dialects.CHOOSE_FROM_ENUM_OR_INPUT
 
     @property
-    def _enum2dict(self) -> dict:
-        res = {}
+    def extra_keys(self) -> dict[str, str]:
+        res = super().extra_keys
 
-        for item in enum._member_map_.values():
+        for item in self.field.type_._member_map_.values():
             res[item.name] = (
-                item.value if not isinstance(item, DescriptiveEnum) 
+                str(item.value) if not isinstance(item, DescriptiveEnum) 
                 else item.description
             )
 
         return res
-
-    def _get_keyboard(self, builder: Optional[InlineKeyboardBuilder] = None):
-        builder = builder or InlineKeyboardBuilder()
-
-        for name, description in self._enum2dict(self.field.type_).items():
-            builder.button(text=description, callback_data=f"{self.item_callback_data}:{name}")
-
-        return super()._get_keyboard(builder)
 
