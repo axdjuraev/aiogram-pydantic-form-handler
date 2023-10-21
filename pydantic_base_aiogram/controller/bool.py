68e073e7edbe4e9b77eadf996a10924409a785d1
axdjuraev <axdjuraev@gmail.com>
2023-10-21 23:26:04 +0500

    ref: BoolController

--- pydantic_base_aiogram/controller/bool.py
+++ pydantic_base_aiogram/controller/bool.py
@@ -1,7 +1,13 @@
-from .enum.single import SingleValueEnumController
+from aiogram.types import CallbackQuery
+from pydantic_base_aiogram.exceptions import DataValidationError
+from pydantic_base_aiogram.types import Event
+from .base import BaseController
 
 
-class BoolController(SingleValueEnumController):
-    def _validate_data(self, data):
-        return str(data).strip() == "1"
+class BoolController(BaseController):
+    async def format_data(self, self_, event: Event[CallbackQuery], *_):
+        try:
+            return str(event._event.data).split(':')[1].strip() == "1"
+        except IndexError:
+            raise DataValidationError(self.dialects.INVALID_TYPE_DATA)
 
