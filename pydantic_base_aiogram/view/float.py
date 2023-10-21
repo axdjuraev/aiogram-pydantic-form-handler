3e0b3a3a92b59b1fec8f4785d3b9214f02f39761
axdjuraev <axdjuraev@gmail.com>
2023-10-22 01:23:26 +0500

    ref (FloatView): add view_text_format

--- pydantic_base_aiogram/view/float.py
+++ pydantic_base_aiogram/view/float.py
@@ -2,10 +2,7 @@ from .base import BaseView
 
 
 class FloatView(BaseView):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.text = (
-            self.field.field_info.extra.get('view_text') 
-            or self.dialects.INPUT_STR.format(field_name=self.field_name)
-        )
+    @property
+    def view_text_format(self):
+        return self.dialects.INPUT_FLOAT
 
