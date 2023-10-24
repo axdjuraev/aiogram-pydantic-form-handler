8cd1a4326cb96c059f81095fa56073616a753ca5
axdjuraev <axdjuraev@gmail.com>
2023-10-24 23:46:09 +0500

    fix (utils:abstractions:is_list_type): generic union checking

--- pydantic_base_aiogram/utils/abstractions.py
+++ pydantic_base_aiogram/utils/abstractions.py
@@ -1,12 +1,15 @@
-from typing import get_origin
+from typing import get_args, get_origin, Union
 from inspect import isclass
 
 
 def is_list_type(obj):
-    type = obj
+    obj_type = obj
 
     if (type_ := get_origin(obj)):
-        type = type_
+        if type_ is not Union:
+            obj_type = type_
+        elif (type_ := get_origin((args := get_args(obj)) and args[0])):
+            obj_type = type_
         
-    return isclass(obj) and issubclass(type, list)
+    return isclass(obj_type) and issubclass(obj_type, list)
 
