4f4f45b31b5d33253fab2e3e348d0b2274371d7a
axdjuraev <axdjuraev@gmail.com>
2023-11-20 07:04:30 +0500

    Revert "feat: FieldNormalizer"
    This reverts commit 47f8ade7fcfe64b692f42031313ed42daa8c2c12.

--- pydantic_base_aiogram/utils/field_normalization/__init__.py
+++
@@ -1,9 +0,0 @@
-from .field_normalization import FieldNormalizer
-from .type_normalization import TypeNormilizer
-
-
-__all__ = [
-	'FieldNormalizer',
-	'TypeNormilizer',
-]
-
--- pydantic_base_aiogram/utils/field_normalization/normilized_model_field.py
+++ pydantic_base_aiogram/utils/field_normalization/normilized_model_field.py
@@ -1,16 +1,10 @@
 from pydantic.fields import ModelField
 
 
-class NormalizedModelField:
+class NormalizedModelField(ModelField):
     is_list: bool
     types: list[type]
     unique_types: list[type]
-    
-    def __init__(self, field: ModelField, types: list[type], is_list: bool) -> None:
-        self._field = field
-        self.types = types
-        self.is_list = is_list
-        self.unique_types = sorted(set(self.types), key=lambda x: x.__name__)
     
     @property
     def is_complex(self):
