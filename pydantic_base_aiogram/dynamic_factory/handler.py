039f47d076ccb3b05d4c05362b379e98a871cacc
axdjuraev <axdjuraev@gmail.com>
2023-11-24 09:45:09 +0500

    ref

--- pydantic_base_aiogram/dynamic_factory/handler.py
+++ pydantic_base_aiogram/dynamic_factory/handler.py
@@ -1,7 +1,8 @@
+from pydantic_base_aiogram.dialecsts import RuDialects
 from pydantic_base_aiogram.main import SchemaBaseHandlersGroup
 from .schema import SchemaBuilderSchema
 
 
 class SchemaBuilderHandlers(SchemaBaseHandlersGroup[SchemaBuilderSchema]):
-    pass
+    DIALECTS = RuDialects()
 
