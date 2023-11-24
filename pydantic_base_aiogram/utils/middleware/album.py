d81d29f93e312da6708bf1bdb320705d1e4c6c27
axdjuraev <axdjuraev@gmail.com>
2023-11-24 10:14:52 +0500

    ref: AlbumMessageMiddleware

--- pydantic_base_aiogram/utils/middleware/album.py
+++ pydantic_base_aiogram/utils/middleware/album.py
@@ -3,6 +3,7 @@ from typing import Optional
 from aiogram import BaseMiddleware, Bot
 from aiogram.types import Message
 
+from pydantic_base_aiogram.types import FType
 from pydantic_base_aiogram.utils.proxy.album_message import ProxyAlbumMessage
 
 
@@ -14,32 +15,30 @@ class AlbumMessageMiddleware(BaseMiddleware):
         super().__init__()
 
     async def __call__(self, handler, message: Message, data):
-        if not message.media_group_id:
+        if message.content_type not in FType:
             return await handler(message, data)
 
         if not self.bot:
             self.bot = data['state'].bot
 
-        doc = self._message2doc(message)
+        key = message.media_group_id or str(message.message_id)
+        doc = message
 
         try:
-            self.album_data[message.media_group_id].append(doc)
+            self.album_data[key].append(doc)
         except KeyError:
-            self.album_data[message.media_group_id] = [doc]
+            self.album_data[key] = [doc]
             await asyncio.sleep(self.latency)
 
             data['_is_last'] = True
-            message = self._get_proxified_msg(message, self.album_data[message.media_group_id])
+            message = self._get_proxified_msg(message, self.album_data[key])
             await handler(message, data)
 
         if message.media_group_id and data.get("_is_last"):
-            del self.album_data[message.media_group_id]
+            del self.album_data[key]
             del data['_is_last']
 
         return await super().__call__(handler, message, data)
-
-    def _message2doc(self, message: Message):
-        return message
 
     def _get_proxified_msg(self, message: Message, album: list):
         return ProxyAlbumMessage(
