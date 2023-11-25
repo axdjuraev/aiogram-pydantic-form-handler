7b7d9ab9295ab89733fb7ac99734b3efa0ce5d81
axdjuraev <axdjuraev@gmail.com>
2023-11-25 15:00:23 +0500

    ref (FieldMetadata): add extra keys

--- pydantic_base_aiogram/dynamic/field_metadata.py
+++ pydantic_base_aiogram/dynamic/field_metadata.py
@@ -9,9 +9,12 @@ class FieldMetadata:
         view_text: Optional[str] = None,
         field_name: Optional[str] = None,
         getter_name: Optional[str] = None,
+        extra_keys: Optional[list[str]] = None,
     ) -> None:
         self._type = type
         self._view_text = view_text
         self._field_name = field_name or str(uuid4())
         self._getter_name = getter_name
+        self._extra_keys = dict(zip(extra_keys, extra_keys)) if extra_keys else None
+
 
