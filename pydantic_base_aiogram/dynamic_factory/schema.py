a816bae0164363f26245b76f40bb1062e7b87df9
axdjuraev <axdjuraev@gmail.com>
2023-11-25 13:19:03 +0500

    ref (InputSchema): add field_name

--- pydantic_base_aiogram/dynamic_factory/schema.py
+++ pydantic_base_aiogram/dynamic_factory/schema.py
@@ -3,6 +3,7 @@ from .enums import InputFormTypesEnum
 
 
 class InputSchema(BaseModel):
+    field_name: str = Field(view_text='Введите название поля')
     view_text: str = Field(view_text='Введите отображаемый текст')
     type: InputFormTypesEnum = Field(view_text='Выберите тип полья')
     is_list: bool = Field(view_text='Является ли данный тип списком')
