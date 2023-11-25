c35abf86d94723aa12ab209c65e5647a59b3a394
axdjuraev <axdjuraev@gmail.com>
2023-11-25 14:58:09 +0500

    ref (InputSchema): add extra_keys

--- pydantic_base_aiogram/dynamic_factory/schema.py
+++ pydantic_base_aiogram/dynamic_factory/schema.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 from .enums import InputFormTypesEnum 
 
@@ -7,6 +8,10 @@ class InputSchema(BaseModel):
     view_text: str = Field(view_text='Введите отображаемый текст')
     type: InputFormTypesEnum = Field(view_text='Выберите тип полья')
     is_list: bool = Field(view_text='Является ли данный тип списком')
+    extra_keys: Optional[list[str]] = Field(
+        view_text='Введите дополнительные кнопки разделенные новой строкой',
+        seperator_symbol=('\n', 'New Line')
+    )
 
 
 class SchemaBuilderSchema(BaseModel):
