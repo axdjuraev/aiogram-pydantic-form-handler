a0f7c7aba7ee9f4e2e1bfb7041d32f9d9b0eac4a
axdjuraev <axdjuraev@gmail.com>
2024-01-25 13:19:45 +0500

    ref (schema:InputSchema): made is_list optional

--- pydantic_base_aiogram/dynamic_factory/schema.py
+++ pydantic_base_aiogram/dynamic_factory/schema.py
@@ -7,8 +7,9 @@ class InputSchema(BaseModel):
     field_name: str = Field(view_text='Введите название поля')
     view_text: str = Field(view_text='Введите отображаемый текст')
     type: InputFormTypesEnum = Field(view_text='Выберите тип полья')
-    is_list: bool = Field(view_text='Является ли данный тип списком')
+    is_list: bool = Field(False, view_text='Является ли данный тип списком')
     extra_keys: Optional[str] = Field(view_text='Введите дополнительные кнопки разделенные новой строкой')
+    pre_next_method: Optional[str] = None
 
 
 class SchemaBuilderSchema(BaseModel):
