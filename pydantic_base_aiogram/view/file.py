d16ae741fb0626bf0f0c266114539417c85fafa9
axdjuraev <axdjuraev@gmail.com>
2024-01-26 11:50:46 +0500

    feat (FileView): add_more_view

--- pydantic_base_aiogram/view/file.py
+++ pydantic_base_aiogram/view/file.py
@@ -1,3 +1,7 @@
+from aiogram import Router
+from aiogram.filters.state import StateFilter
+
+from pydantic_base_aiogram.types import Event
 from .base import BaseView
 
 
@@ -10,4 +14,16 @@ class FileView(BaseView):
             return self.dialects.SEND_FILES
 
         return self.dialects.SEND_FILE
+    
+    async def add_more_view(self, event_type, state):
+        event = Event(event_type)
+        keyboard = await self.get_dynamic_keyboard(state, ignore_list=False)
+        await event.answer(
+            self.dialects.WAITING_EXTRA_FILES, 
+            reply_markup=keyboard.as_markup(), # type: ignore
+        )
+
+    def register2router(self, router: Router) -> Router:
+        router.message(StateFilter(self.state))(self.add_more_view)
+        return super().register2router(router)
 
