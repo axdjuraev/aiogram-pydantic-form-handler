9eb870560dfb9adc672a766dd6a24500a2cf8cce
axdjuraev <axdjuraev@gmail.com>
2024-01-26 13:25:08 +0500

    ref (BaseView:add_more_view): remove previous view text keyboard

--- pydantic_base_aiogram/view/file.py
+++ pydantic_base_aiogram/view/file.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 from aiogram.filters.state import StateFilter
+from aiogram.fsm.context import FSMContext
 
 from pydantic_base_aiogram.types import Event
 from .base import BaseView
@@ -15,13 +16,22 @@ class FileView(BaseView):
 
         return self.dialects.SEND_FILE
     
-    async def add_more_view(self, event_type, state):
+    async def add_more_view(self, event_type, state: FSMContext):
+        state_data = await state.get_data()
+
+        if (last_view_messsage_id := await self._get_last_view_msg_id(state_data)):
+            try:
+                await state.bot.edit_message_reply_markup(
+                    chat_id=state.key.chat_id,
+                    message_id=last_view_messsage_id, 
+                    reply_markup=None,
+                )
+            except Exception:
+                pass
+        
         event = Event(event_type)
         keyboard = await self.get_dynamic_keyboard(state, ignore_list=False)
-        await event.answer(
-            self.dialects.WAITING_EXTRA_FILES, 
-            reply_markup=keyboard.as_markup(), # type: ignore
-        )
+        await self._show_view(event, state, self.dialects.WAITING_EXTRA_FILES, keyboard.as_markup())
 
     def register2router(self, router: Router) -> Router:
         router.message(StateFilter(self.state))(self.add_more_view)
