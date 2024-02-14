be02eb273e2eef2808febf3fb4de39c56b15b2be
axdjuraev <axdjuraev@gmail.com>
2024-02-14 09:10:53 +0500

    ref (utils:ProxyAlbumMessage): use commbination of html_content while copy_to

--- pydantic_base_aiogram/utils/proxy/album_message.py
+++ pydantic_base_aiogram/utils/proxy/album_message.py
@@ -2,7 +2,6 @@ from typing import Optional
 from aiogram import Bot
 from aiogram.types import Message
 from pydantic import BaseModel, Field
-
 from pydantic_base_aiogram.utils.middleware.type_album import Album
 
 
@@ -19,7 +18,8 @@ class ProxyAlbumMessage(Message, BaseModel):
             reply_id = kwargs.get('reply_to_message_id')
 
             if 'caption' in kwargs:
-                msg = await self.bot.send_message(chat_id, kwargs.pop('caption'), **kwargs)
+                text = '<br/>'.join(self.album.html_contents)
+                msg = await self.bot.send_message(chat_id, text, **kwargs)
                 reply_id = msg.message_id
 
             media = [x.get_as_input_media() for x in self.album._items]
