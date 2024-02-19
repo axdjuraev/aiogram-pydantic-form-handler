66d7ac8f9dd0b845c7b75304bc9c8682a4541362
axdjuraev <axdjuraev@gmail.com>
2024-02-19 07:42:19 +0500

    feat (dynamic): SchemaFieldAddHandler

--- pydantic_base_aiogram/dynamic_factory/handler.py
+++ pydantic_base_aiogram/dynamic_factory/handler.py
@@ -1,6 +1,6 @@
 from pydantic_base_aiogram.dialecsts import RuDialects
 from pydantic_base_aiogram.main import SchemaBaseHandlersGroup
-from .schema import SchemaBuilderSchema
+from .schema import InputSchema, SchemaBuilderSchema
 
 
 class SchemaBuilderHandlers(SchemaBaseHandlersGroup[SchemaBuilderSchema]):
@@ -11,3 +11,9 @@ class SchemaBuilderHandlers(SchemaBaseHandlersGroup[SchemaBuilderSchema]):
         "fields_pre_next_method",
     ]
 
+
+class SchemaFieldAddHandler(SchemaBaseHandlersGroup[InputSchema]):
+    _except_steps = [
+        "is_list",
+        "pre_next_method",
+    ]
